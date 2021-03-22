69bf96fe4b516b4d8c427a9df052b24c5a4f5e19
Pragnyayadav <63871463+Pragnyayadav@users.noreply.github.com>
2021-03-22 11:01:18 +0530

    .

---
+++ 05_practice.py
@@ -0,0 +1,75 @@
+# Question 1
+# myDict = {"Namaste" : "Hello",
+# "Aap kese hain": "How are you?",
+# "Tumhara naam kya hai": "What's your name"}
+# 
+# b = input("Enter hindi: ")
+# print(myDict.get(b))
+
+# Question 2
+# s = [90, 80, 90]
+# p = set(s)
+# q = list(p)
+# print(p)
+
+
+
+# Question 3
+# mySet = (3,"3")
+# print(mySet)
+
+
+
+# Question 4
+# s = set()
+# 
+# s.add(20)
+# 
+# s.add(20.0)
+# 
+# s.add("20")
+# print(len(s))
+
+
+
+# Question 5
+
+# num1 = input("Enter: ")
+# num2 = input("Enter: ")
+# num3 = input("Enter: ")
+# num4 = input("Enter: ")
+# num5 = input("Enter: ")
+# num6 = input("Enter: ")
+# 
+# mySet = set()
+# mySet.add(num1)
+# mySet.add(num2)
+# mySet.add(num3)
+# mySet.add(num4)
+# mySet.add(num5)
+# mySet.add(num6)
+# 
+# print(mySet)
+
+
+# Question 6
+
+# s = {}
+# print(type(s))
+
+
+# Question 7
+
+favLang = {}
+a = input("Enter: ")
+b = input("Enter: ")
+c = input("Enter: ")
+d = input("Enter: ")
+
+
+favLang['Shubham'] = a
+favLang['Shivani'] = b
+favLang['Sheela'] = c
+favLang['Susheela'] = d
+
+print(favLang.items())
